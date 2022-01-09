9449493eea7811d8c9f05e273e1dcde77aefd6f7
josef <josheshin@gmail.com>
2022-01-09 18:00:24 -0300

    feat: primera versión

---
+++ install_sys.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+set -v
+
+# Never run pacman -Sy on your system!
+pacman -Sy dialog
+
+timedatectl set-ntp true
+
+dialog --defaultno --title "Are you sure?" --yesno \
+    "This is my personnal arch linux install. \n\n\
+    It will DESTROY EVERYTHING on one of your hard disk. \n\n\
+    Don't say YES if you are not sure what you're doing! \n\n\
+    Do you want to continue?" 15 60 || exit
+
+# dialog usa ncurses que dibuja sin usar el xserver, usando stdout y usa stderr como stdout, por eso el 2>
+dialog --no-cancel --inputbox "Enter a name for your computer." 10 60 2> comp
+
+# Verify boot (UEFI or BIOS)
+uefi=0
+ls /sys/firmware/efi/efivars 2> /dev/null && uefi=1
+
+devices_list=($(lsblk -d | awk '{print "/dev/" $1 " " $4 " on"}' \
+| grep -E 'sd|hd|vd|nvme|mmcblk'))
+
+dialog --title "Choose your hard drive" --no-cancel --radiolist \
+    "Where do you want to install your new system? \n\n\
+    Select with SPACE, valid with ENTER. \n\n\
+    WARNING: Everything will be DESTROYED on the hard disk!" \
+15 60 4 "${devices_list[@]}" 2> hd
+
+hd=$(cat hd) && rm hd
+
+default_size="8"
+dialog --no-cancel --inputbox \
+    "You need three partitions: Boot, Root and Swap \n\
+    The boot partition will be 512M \n\
+    The root partition will be the remaining of the hard disk \n\n\
+    Enter below the partition size (in Gb) for the Swap. \n\n\
+    If you don't enter anything , it will default to ${default_size}G. \n" \
+20 60 2> swap_size
+
+size=$(cat swap_size) && rm swap_size
+
+[[ $size =~ ^[0-9]+$ ]] || size=$default_size
+
+boot_partition_type=1
+[[ "$uefi" == 0 ]] && boot_partition_type=4
+
+
+# Create the partitions
+
+#g - create non empty GPT partition table
+#n - create new partition
+#p - primary partition
+#e - extended partition
+#w - write the table to disk and exit
+
+partprobe "$hd"
+
+fdisk "$hd" << EOF
+g
+n
+
+
++512M
+t
+$boot_partition_type
+n
+
+
++${size}G
+n
+
+
+
+w
+EOF
+
+partprobe "$hd"
+
+# Format the partitions
+
+mkswap "${hd}2"
+swapon "${hd}2"
+mkfs.ext4 "${hd}3"
+mount "${hd}3" /mnt
+
+if [ "$uefi" = 1 ]; then
+    mkfs.fat -F32 "${hd}1"
+    mkdir -p /mnt/boot/efi
+    mount "${hd}1" /mnt/boot/efi
+fi
+
+# Install Arch Linux! Glory and fortune!
+pacstrap /mnt base base-devel linux linux-firmware
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Persist important values for the next script
+echo "$uefi" > /mnt/var_uefi
+echo "$hd" > /mnt/var_hd
+mv comp /mnt/comp
+
+# Don't forget to replace "Phantas0s" by the username of your Github account
+curl https://raw.githubusercontent.com/josef-o\
+/arch_installer/master/install_chroot.sh > /mnt/install_chroot.sh
+
+arch-chroot /mnt bash install_chroot.sh
+
+rm /mnt/var_uefi
+rm /mnt/var_hd
+rm /mnt/install_chroot.sh
+rm /mnt/comp
+
+dialog --title "To reboot or not to reboot?" --yesno \
+    "Congrats! The install is done! \n\n\
+    Do you want to reboot your computer?" 20 60
+
+response=$?
+
+case $response in
+    0) reboot;;
+    1) clear;;
+esac
